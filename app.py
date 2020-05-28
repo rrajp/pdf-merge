2bd0b97589eec6b972cf02e927d91ff5144c8b5e
Raviraj <raviraj.prajapat@sirionlabs.com>
2020-05-29 00:38:01 +0530

    logging debug 3

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ i = 0
 
 @app.route('/', methods = ['GET', 'POST'])
 def front():
-    app.logger.info("Here 0")
+    app.logger.debug("Here 0")
     global i
     i += 1
     if request.method == "POST":
@@ -18,7 +18,7 @@ def front():
         try:
             get_dict = dict()
             flag = True
-            app.logger.info("Here 1")
+            app.logger.debug("Here 1")
             for k, v in request.files.items():
                 number = k.split("_")[1]
                 pages_range = k.split("_")[2]
@@ -26,7 +26,7 @@ def front():
                 file = v
                 if priority == "":
                     flag = False
-            app.logger.info("Here 2")
+            app.logger.debug("Here 2")
             for k, v in request.files.items():
                 if flag:
                     priority = k.split("_")[3]
@@ -46,7 +46,7 @@ def front():
                             get_dict[int(priority)]['pages'] += list(range(start, end + 1))
                         else:
                             get_dict[int(priority)]['pages'].append(int(page))
-            app.logger.info("Here 3")
+            app.logger.debug("Here 3")
 
             pd_w = PdfFileWriter()
             for key in sorted(list(get_dict.keys())):
@@ -57,10 +57,10 @@ def front():
                             pd_w.addPage(read.getPage(allpage))
                     else:
                         pd_w.addPage(read.getPage(pg - 1))
-            app.logger.info("Here 4")
+            app.logger.debug("Here 4")
             with open(os.path.join(current_app.root_path, f'static/download/result_{i}.pdf'), 'wb') as out:
                 pd_w.write(out)
-            app.logger.info(f"Saved 'result_{i}.pdf'")
+            app.logger.debug(f"Saved 'result_{i}.pdf'")
             return f'result_{i}.pdf'
         except Exception as e:
             return "Invalid Input"
